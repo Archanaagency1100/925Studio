809a3b83508066d8aa30df3f16e0776ee0feb9ae
Archanaagency1100 <archana@1100.in>
2025-11-21 16:23:22 +0530

    Initial commit

---
+++ Static/js/script.js
@@ -0,0 +1,8 @@
+const navToggle = document.getElementById("navToggle");
+const navMenu = document.getElementById("navMenu");
+
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
